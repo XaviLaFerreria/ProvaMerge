75a41dd0824c98998cdf07a6dd429c54622874bc
adrivferreria <101583803+adrivferreria@users.noreply.github.com>
2022-04-04 17:13:12 +0200

    Add files via upload

---
+++ ProvaMerge/src/origin/main.java
@@ -0,0 +1,11 @@
+package origin;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+    }
+    
+}
