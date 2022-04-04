15f588eae0edd094e1bc53f87a29ba68c4e6ba1b
Xavi <Xavi@DESKTOP-606OICB>
2022-04-04 17:52:10 +0200

    Union de branch a y b

--- ProvaMerge/src/origin/main.java
+++ ProvaMerge/src/origin/main.java
@@ -8,6 +8,9 @@ public class main {
         System.out.println("c");
         System.out.println("d");
         System.out.println("e");
+        
+        
+        System.out.println("f");
     }
     
 }
